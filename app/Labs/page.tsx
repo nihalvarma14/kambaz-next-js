b56a36658d29ff99b11e45f85d744dcec5d4cb8c
Venkata Sai Siva Nihalvarma Pericherla <pericherlanihalvarma@gmail.com>
2025-09-22 16:24:30 -0400

    Added name in Labs/page.tsx

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,7 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <h2>Venkata Sai Siva Nihalvarma Pericherla</h2>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
